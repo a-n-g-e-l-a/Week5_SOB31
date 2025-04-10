1790e9ff18b8705c9a5cd104ae78178667cac71b
a-n-g-e-l-a <143579067+a-n-g-e-l-a@users.noreply.github.com>
2025-04-10 16:29:57 +0400

    Update broken_pirate.py

--- broken_pirate.py
+++ broken_pirate.py
@@ -1,5 +1,10 @@
-greeting = input("Hello, possible pirate! What's the password?)
-if greeting in ["Arrr!"):
-	print("Go away, pirate.")
-elif
-print("Greetings, hater of pirates!")
+# I added proper parentheses and colons, fixed logic issues
+greeting = input("Hello, possible pirate! What's the password?")  # I added input for pirate password
+
+# I fixed mismatched parentheses and incomplete condition
+if greeting in ["Arrr!"]:  # I changed the incorrect list syntax (missed closing parenthesis)
+    print("Go away, pirate.")  # I added this line to print if password matches "Arrr!"
+
+# I fixed: 'elif' was incomplete, added correct condition and print statement
+else:  # I added 'else' to handle the case when the password is not "Arrr!"
+    print("Greetings, hater of pirates!")  # I added this line to greet non-pirates
